2822f2f9850a14c8d5fd89cb12b191f1b70bb632
Saiweb3dev <saik434455@gmail.com>
2025-02-28 21:37:31 +0530

    feat : updated new routes to handle tasks from client

--- backend/task-manager/routes/index.js
+++ backend/task-manager/routes/index.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const controller = require("../controllers/controllerEndpoint");
 
 router.get("/test", controller.testController.test);
-router.post("/tasks",controller.taskController.createTask);
+
+// Task routes
+router.post("/task",controller.taskController.createTask);
+router.get("/task/:taskId",controller.taskController.getTaskById);
+router.get("/task/user/:userId",controller.taskController.getTasksByUserId);
+router.put("/task/:taskId",controller.taskController.updateTask);
+router.delete("/task/:taskId",controller.taskController.deleteTask);
+
 
 module.exports = router;
